a27ccf2f00eff05878bd63d91863ebf7822fe538
Tumise <oladele.alade@gmail.com>
2021-05-14 19:27:42 +0100

    chore(user) added profile model

--- user/models.py
+++ user/models.py
@@ -6,10 +6,19 @@ import uuid
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from utils.mixins import TimeStampMixin
+
+
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     preferred_name = models.CharField(max_length=250)
 
 
 class Profile(TimeStampMixin):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user_profile")
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user_profile"
+    )
+    dob = models.DateField()
+    country_code = models.CharField(max_length=3)
+    contact_number = models.CharField(
+        max_length=10, help_text="start number after country code"
+    )
