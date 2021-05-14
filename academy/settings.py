3a74c66206d96c5c0be91830ef7f2562c66a3ca0
Tumise <oladele.alade@gmail.com>
2021-05-14 20:48:27 +0100

    added oauth to middleware

--- academy/settings.py
+++ academy/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "oauth2_provider.middleware.OAuth2TokenMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
