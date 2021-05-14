c1e9eec9851dbf95784b8ba2e021e80c32556bf8
Tumise <oladele.alade@gmail.com>
2021-05-14 19:19:46 +0100

    chore (user) added user profile

--- user/models.py
+++ user/models.py
@@ -5,7 +5,11 @@ import uuid
 
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
-
+from utils.mixins import TimeStampMixin
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     preferred_name = models.CharField(max_length=250)
+
+
+class Profile(TimeStampMixin):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="user_profile")
