04223e1159a887c0f6901982a742f44e9e62d1eb
Tumise <oladele.alade@gmail.com>
2021-05-14 20:33:14 +0100

    aded django corsheaders

--- academy/settings.py
+++ academy/settings.py
@@ -47,6 +47,7 @@ THIRD_PARTY_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "oauth2_provider",
+    "corsheaders",
 ]
 INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
