486978cf3f0c89fbfd3b272cf9fdad2403cf15c5
Tumise <oladele.alade@gmail.com>
2021-05-14 19:44:21 +0100

    added authentication backends

--- academy/settings.py
+++ academy/settings.py
@@ -120,3 +120,7 @@ STATIC_URL = "/static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "user.User"
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+]
