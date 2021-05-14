da7e83c1c8035b8481df42e411266d74f1b8647d
Tumise <oladele.alade@gmail.com>
2021-05-14 20:54:40 +0100

    added rest framework diffualt permission classes

--- academy/settings.py
+++ academy/settings.py
@@ -150,7 +150,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "oauth2_provider.contrib.rest_framework.OAuth2Authentication",
-    )
+    ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
 }
 OAUTH2_PROVIDER = {
     # this is the list of available scopes
