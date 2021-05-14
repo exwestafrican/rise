945f027b780ac45b9460837f4923264a8fe1243d
Tumise <oladele.alade@gmail.com>
2021-05-14 19:14:27 +0100

    chore(user) added an abstract base user

--- user/models.py
+++ user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+import uuid
+
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    preferred_name = models.CharField(max_length=250)
